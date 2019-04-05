9cbac329518f51695fc6f700ceff144d6c699341
aappleton8 <aappleton8@users.noreply.github.com>
2019-04-05 22:31:43 +0100

    Rename 'opts' to 'options'
    - Rename 'opts' to 'options' for clarity.
    - Update the documentation.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Authentication Consumer Middleware Library
 ## Description
-Library to ease the shift to becoming an authentication consumer
+Library code for an authentication consumer. This library is to be imported in to an euthentication consumer program. 
+
+## Usage
+This library is designed to be incorported in to an authentication consumer. A usage example is given below. 
+```
+app.use(authenticationConsumer({ options }))
+```
+`options` is an object with three parts: 
+- `tokenService: { endpoint }` - The hostname of the token service to use. 
+- `authenticationProvider: { endpoint, consumerId }` - The hostname of the provider to use and the consumer uuid used by the provider to identify the consumer. 
+- `authenticationConsumer: { endpoint }` - The hostname of the consumer. 
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const fetch = require('node-fetch')
 
-module.exports = function(opts) {
-  // opts should have
+module.exports = function(options) {
+  // options should have
   // {
   //   tokenService: { endpoint },
   //   authenticationProvider: { endpoint, consumerId },
@@ -11,30 +11,30 @@ module.exports = function(opts) {
 
   const router = express.Router()
   const getLogin = (url) => {
-    return `${opts.authenticationProvider.endpoint}/api/auth?id=${opts.authenticationProvider.consumerId}&redirect=${opts.authenticationConsumer.endpoint}/login_callback%3Fto=%3D${encodeURIComponent(url)}`
+    return `${options.authenticationProvider.endpoint}/api/auth?id=${options.authenticationProvider.consumerId}&redirect=${options.authenticationConsumer.endpoint}/login_callback%3Fto=%3D${encodeURIComponent(url)}`
   }
 
   const getLogout = (url) => {
-    return `${opts.authenticationProvider.endpoint}/logout?to=${encodeURIComponent(url)}`
+    return `${options.authenticationProvider.endpoint}/logout?to=${encodeURIComponent(url)}`
   }
 
   router.use((req, res, next) => {
     res.locals.auth = { getLogin, getLogout }
     res.locals.location = { url: req.protocol + '://' + req.get('host') + req.originalUrl }
     if (req.session.token) {
-      fetch(`${opts.tokenService.endpoint}?token=${req.session.token}`)
-        .then(resp => resp.json())
-        .then(resp => {
-          if ('err' in resp) {
-            if (resp.err === 'Token not found') {
-              req.session.token = undefined
-              req.session.user = undefined
-              return next()
-            }
+      fetch(`${options.tokenService.endpoint}?token=${req.session.token}`)
+      .then(resp => resp.json())
+      .then(resp => {
+        if ('err' in resp) {
+          if (resp.err === 'Token not found') {
+            req.session.token = undefined
+            req.session.user = undefined
+            return next()
           }
-          req.session.user = resp
-          next()
-        })
+        }
+        req.session.user = resp
+        next()
+      })
     } else {
       next()
     }
@@ -46,7 +46,8 @@ module.exports = function(opts) {
   })
 
   router.get('/login_callback', (req, res) => {
-    const { token } = req.query
+    const { token, to } = req.query
+    //console.log(decodeURIComponent(to))
     req.session.token = token
     res.redirect('/')
   })
